b3b55b0250fbf265757a733257d563ad04412b4e
bolsillolleno <camilo.martinez260597@gmail.com>
2026-04-06 18:13:58 -0500

    Add files via upload

--- src/utils/delay.js
+++ src/utils/delay.js
@@ -1,1 +1,11 @@
+exports.humanDelay = (min, max) => {
+  const delay = Math.floor(Math.random() * (max - min + 1)) + min;
+  return new Promise(resolve => setTimeout(resolve, delay));
+};
 
+exports.typingSimulation = async (sock, jid, duration = 2000) => {
+  await sock.presenceSubscribe(jid);
+  await sock.sendPresenceUpdate('composing', jid);
+  await new Promise(r => setTimeout(r, duration));
+  await sock.sendPresenceUpdate('paused', jid);
+};
