df0a5155676d203c97734630aa43585f226a0ff7
bolsillolleno <camilo.martinez260597@gmail.com>
2026-04-06 18:06:50 -0500

    Add files via upload

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,1 +1,11 @@
+const admin = require('firebase-admin');
 
+// Usar credenciales de entorno (Railway vars)
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DB_URL
+});
+
+module.exports = admin;
